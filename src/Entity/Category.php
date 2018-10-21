324c9c85bfdd96295299d34919c1d8f69ee11ce9
Cesar Bretana Gonzalez <bretanac@gmail.com>
2018-10-21 14:49:08 +0200

    Initial commit

---
+++ src/Entity/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=7)
+     */
+    private $internal_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInternalId(): ?string
+    {
+        return $this->internal_id;
+    }
+
+    public function setInternalId(string $internal_id): self
+    {
+        $this->internal_id = $internal_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
